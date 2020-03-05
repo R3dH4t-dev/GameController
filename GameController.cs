325a69b2c6c92a578cb2f67afa8a86f67d195d5f
R3dH4t-dev <61518409+R3dH4t-dev@users.noreply.github.com>
2020-03-05 14:50:59 -0300

    Add files via upload

---
+++ GameController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public Transform playerTransform;
+    private Camera cam;
+    
+    public Transform limit_Left, limit_Right, limit_Top, limit_Bot;
+    public float speedCam;
+
+    void CamController()
+    {
+        float posCamX = playerTransform.position.x;
+        float posCamY = playerTransform.position.y;
+
+        if (cam.transform.position.x < limit_Left.position.x && playerTransform.position.x < limit_Left.position.x)
+        {
+            posCamX = limit_Left.position.x;
+        }
+        else if (cam.transform.position.x > limit_Right.position.x && playerTransform.position.x > limit_Right.position.x)
+        {
+            posCamX = limit_Right.position.x;
+        }
+        if (cam.transform.position.y < limit_Bot.position.y && playerTransform.position.y < limit_Bot.position.y)
+        {
+            posCamY = limit_Bot.position.y;
+        }
+        else if (cam.transform.position.y > limit_Top.position.y && playerTransform.position.y > limit_Top.position.y)
+        {
+            posCamY = limit_Top.position.y;
+        }
+
+        Vector3 posCam = new Vector3(posCamX, posCamY, cam.transform.position.z);
+
+        cam.transform.position = Vector3.Lerp(cam.transform.position, posCam, speedCam * Time.deltaTime);
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        CamController();    
+    }
+
+    
+}
---
+++ Player_Control.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Control : MonoBehaviour
+{
+    private Rigidbody2D playerRb;
+    private Animator playerAnimator;
+    private GameController _GameController;
+
+    public float speedX;
+    public float jumpForce;
+
+    public bool isLookLeft;
+
+    public Transform groundCheck;
+    private bool isGrounded;
+    private bool isAttack;
+
+    public GameObject hitboxPreFab;
+    public Transform hand;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent <Animator>();
+        _GameController = FindObjectOfType(typeof(GameController)) as GameController;
+        _GameController.playerTransform = this.transform;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+
+        if(isAttack == true && isGrounded == true)
+        {
+            h = 0;
+        }
+
+        if(h > 0 && isLookLeft == true)
+        {
+            Flip();
+        }
+        else if(h < 0 && isLookLeft == false)
+        {
+            Flip();
+        }
+
+
+        float speedY = playerRb.velocity.y;
+
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
+        {
+            playerRb.AddForce(new Vector2(0, jumpForce));
+            
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            
+            playerAnimator.SetTrigger("attack");
+            isAttack = true;
+        }
+
+
+        playerRb.velocity = new Vector2(h * speedX, speedY );
+        playerAnimator.SetInteger("h", (int)h);
+        playerAnimator.SetBool("isGrounded", isGrounded);
+        playerAnimator.SetFloat("speedY", speedY);
+        playerAnimator.SetBool("isAttack", isAttack);
+
+
+        
+    }
+
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.02f);
+    }
+
+    void Flip()
+    {
+        isLookLeft = !isLookLeft;
+        float x = transform.localScale.x * -1; //Inverte o sinal do X
+        transform.localScale = new Vector3(x, transform.localScale.y, transform.localScale.z);
+
+    }
+
+    void onEndAttack()
+    {
+        isAttack = false; 
+    }
+
+    void hitboxAttack()
+    {
+        GameObject hitboxTemp = Instantiate(hitboxPreFab, hand.position, transform.localRotation);
+        Destroy(hitboxTemp, 0.02f);
+    }
+
+    
+}
